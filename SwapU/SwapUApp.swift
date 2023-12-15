aed120b9e989f4695b8d154e6294f78f4ce6b152
Nick Frazier <Fraziernick894@gmail.com>
2023-12-15 17:43:35 -0500

    Initial Commit

---
+++ SwapU/SwapUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapUApp.swift
+//  SwapU
+//
+//  Created by Nick Frazier on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwapUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
